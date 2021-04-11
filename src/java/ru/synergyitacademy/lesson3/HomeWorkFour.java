2b463c1575be2bfe4db90f3dfa664343f259a48f
Vasiliy <vazyl@mail.ru>
2021-04-12 00:20:18 +0300

    Урок 3 Чигиров Василий

--- src/java/ru/synergyitacademy/lesson3/HomeWorkFour.java
+++ src/java/ru/synergyitacademy/lesson3/HomeWorkFour.java
@@ -1,4 +1,23 @@
 package ru.synergyitacademy.lesson3;
 
+import java.util.Scanner;
+
 public class HomeWorkFour {
+    public static void main(String[] args) {
+        // a+b=c   b+c=d
+        float a = 0;
+        float b = 1;
+        float c = 1;
+        float fibonachchi=c;
+        System.out.println("Введите N-Число последовательности Чисел Фибоначчи:");
+        Scanner scanner = new Scanner(System.in);
+        float number = scanner.nextInt();
+        for ( int i = 1; i <= number; i++){
+            fibonachchi = c;
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        System.out.println(fibonachchi);
+    }
 }
